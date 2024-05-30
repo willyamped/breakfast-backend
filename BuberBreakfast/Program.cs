fc87000939545ab7a6f399de2f04acb7c35ae234
Willy Angga <ampedwilly@gmail.com>
2024-05-30 11:36:09 +0700

    Replace db credentials with environment variables

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -2,13 +2,21 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BuberBreakfast.Data;
+using dotenv.net;
+
+DotEnv.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
     builder.Services.AddScoped<IBreakfastService, BreakfastService>();
+
+    var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+    var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
     builder.Services.AddDbContext<BreakfastDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection")
+    .Replace("{DB_USER}", dbUser)
+    .Replace("{DB_PASSWORD}", dbPassword),
     new MySqlServerVersion(new Version(7, 0, 0))));
 }
 
